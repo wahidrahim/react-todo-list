1d540ce4c2d4027549947b8efc00e527af74e60c
Wahid Rahim <wahidrahim@gmail.com>
2019-12-16 23:01:29 -0500

    adding new tasks

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [newTodo, setNewTodo] = useState('')
   const [todos, setTodos] = useState([
     {
+      id: 1,
       task: 'example task',
       completed: false
     }
@@ -11,14 +13,27 @@ function App() {
 
   return (
     <div className="App">
-      <input type="text" />
-      <button>Add</button>
+      <input type="text" onChange={e => setNewTodo(e.target.value)} />
+      <button
+        onClick={() => {
+          setTodos([
+            ...todos,
+            {
+              id: todos.length + 1,
+              task: newTodo,
+              completed: false
+            }
+          ])
+        }}
+      >
+        Add
+      </button>
       <ul>
         {todos.map(todo => {
-          return <li>{todo.task}</li>
+          return <li key={todo.id}>{todo.task}</li>
         })}
       </ul>
-      <pre>{JSON.stringify(todos)}</pre>
+      <pre>{newTodo}</pre>
     </div>
   )
 }
