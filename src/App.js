2f98762b549e36854c167e3437786ac9f96d18bb
Wahid Rahim <wahidrahim@gmail.com>
2019-12-16 22:54:22 -0500

    rendering todos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      task: 'example task',
+      completed: false
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" />
+      <button>Add</button>
+      <ul>
+        {todos.map(todo => {
+          return <li>{todo.task}</li>
+        })}
+      </ul>
+      <pre>{JSON.stringify(todos)}</pre>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
