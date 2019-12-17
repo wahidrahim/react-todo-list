975f6ca72de7135fcb4fa1f798e380b679277708
Wahid Rahim <wrahim@engagepeople.com>
2019-12-17 11:10:54 -0500

    adding new tasks

--- src/App.js
+++ src/App.js
@@ -6,26 +6,42 @@ export class App extends React.Component {
 
     this.state = {
       newTask: '',
+      todos: []
+    }
+  }
+
+  setNewTask = e => {
+    this.setState({
+      newTask: e.target.value
+    })
+  }
+
+  addTask = e => {
+    this.setState({
       todos: [
+        ...this.state.todos,
         {
-          id: 1,
-          task: 'example',
+          id: this.state.todos.length + 1,
+          task: this.state.newTask,
           done: false
         }
       ]
-    }
+    })
+
+    e.preventDefault()
   }
 
   render() {
     return (
-      <form>
-        <input type="text" />
-        <button type="submit">Add</button>
-
+      <div id="app">
+        <form onSubmit={this.addTask}>
+          <input type="text" onChange={this.setNewTask} />
+          <button type="submit">Add</button>
+        </form>
         <ul>
           {this.state.todos.map(todo => {
             return (
-              <li>
+              <li key={todo.id}>
                 <span>{todo.task}</span>
                 <button>Done</button>
               </li>
@@ -34,17 +50,17 @@ export class App extends React.Component {
         </ul>
 
         {/* <h4>Completed</h4>
-        <ul>
-          {this.state.todos.map(todo => {
-            return (
-              <li>
-                <span>{todo.task}</span>
-                <button>Done</button>
-              </li>
-            )
-          })}
-        </ul> */}
-      </form>
+      <ul>
+        {this.state.todos.map(todo => {
+          return (
+            <li>
+              <span>{todo.task}</span>
+              <button>Done</button>
+            </li>
+          )
+        })}
+      </ul> */}
+      </div>
     )
   }
 }
